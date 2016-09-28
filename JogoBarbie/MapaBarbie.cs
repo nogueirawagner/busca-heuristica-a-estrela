2eef156fdbc6061f0fcdf5aa9e33c4bf56645e67
Wagner Nogueira <wagner@ephrom.com.br>
2016-09-28 13:58:41 -0300

    Distancia

--- JogoBarbie/MapaBarbie.cs
+++ JogoBarbie/MapaBarbie.cs
@@ -89,10 +89,14 @@ namespace JogoBarbie
     private void VerificarProximoMovimento()
     {
       //Orientação: Norte, Sul, Leste, Oeste
+
+      var linha = 23;
+      var coluna = 19;
       
 
-      matrizG = new int[23, 19];
-
+      matrizG = new int[linha, coluna];
+      var matrizY = new int[linha, coluna];
+      var distancia = PegaDistancia(matrizG, matrizY);
 
 
       var casaBarbie = new PictureBox
@@ -105,6 +109,18 @@ namespace JogoBarbie
 
     }
 
+    private int PegaDistancia(int[,] matriz1, int[,] matriz2)
+    {
+      var matriz = 0;
+      for (int i = 0; i < 1; i++)
+      {
+        for (int j = 0; j < 1; j++)
+        {
+          matriz = matriz1[i, j] - matriz2[i, j];
+        }
+      }
+      return matriz;
+    }
 
     private void btnInicial_Click(object sender, EventArgs e)
     {
